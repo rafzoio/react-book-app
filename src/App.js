0d81e5d541f3c756b0db88ef904783644dfa2ee5
rafzoio <raphael.zoio@hotmail.com>
2023-04-03 11:38:43 +0100

    added navbar component and booklist page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import logo from './logo.svg';
+import BookList from './pages/BookList.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-4xl'>
-          Librarius
+        <p className='text-4xl drop-shadow-sm'>
+          React Library
         </p>
+        <BookList />
       </header>
     </div>
   );
---
+++ src/pages/BookList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BookList = () => {
+  return <div>books</div>;
+};
+
+export default BookList;
