b268a4b9e528a545cff4c0735ccd964d94bea446
rafzoio <raphael.zoio@hotmail.com>
2023-04-03 12:41:24 +0100

    Added all pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import './App.css';
-import logo from './logo.svg';
-import BookList from './pages/BookList.jsx';
+import Footer from "./components/Footer";
+import Navbar from './components/Navbar';
+import About from "./pages/About";
+import AddBook from "./pages/AddBook";
+import BookList from "./pages/BookList";
+import Detail from "./pages/Detail";
+import Home from "./pages/Home";
+import UpdateBook from "./pages/UpdateBook";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-4xl drop-shadow-sm'>
-          React Library
-        </p>
-        <BookList />
-      </header>
+      <Router>
+      <Navbar />
+        <div className="container">
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/books" element={<BookList />} />
+            <Route path="/add" element={<AddBook />} />
+            <Route path="/add" element={<UpdateBook />} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const BookList = () => {
-  return <div>books</div>;
+  const [bookList, setBookList] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8081/book-api/book-api?id=1000",
+          {
+            headers: {
+              Accept: "application/json",
+            },
+          }
+        );
+        setBookList(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchBooks();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-large">All Books</h1>
+      {bookList.map((book) => (
+        <div key={book.id}>
+          <h2>{book.title}</h2>
+          <p>{book.author}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default BookList;
