6644a492fc67ff5135f3c9ea1257dfe60ef20f57
rafzoio <raphael.zoio@hotmail.com>
2023-04-05 20:22:43 +0100

    Added about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import BookIcon from "../resources/book.svg";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="flex flex-col align-middle mx-auto w-full items-center">
+      <img src={BookIcon} className="h-24 mt-10" alt="Logo" />
+      <h2 className="font-bold text-white">Designed by Raphael Zoio</h2>
+      <button className="mt-5 w-36 text-black focus:ring-4 focus:outline-none font-bold rounded-lg text-medium py-2.5 text-center bg-gray-200 hover:bg-gray-400">
+        <Link
+          to="https://github.com/rafzoio/react-book-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </Link>
+      </button>
+    </div>
+  );
 };
 
 export default About;
