fe8cb656e9fc640b3268cdee9f4ed4924c78e5a8
rafzoio <raphael.zoio@hotmail.com>
2023-04-27 16:20:21 +0100

    Added settings to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,6 +67,19 @@ const Navbar = () => {
               </li>
               <li>
                 <Link
+                  to="/settings"
+                  className={
+                    activeLink === "Settings"
+                      ? "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                      : "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  }
+                  onClick={handleNavLinkClick}
+                >
+                  Settings
+                </Link>
+              </li>
+              <li>
+                <Link
                   to="/about"
                   className={
                     activeLink === "About"
