906b2b5043e1972a2898d15d4ee124a5f792e2fe
rafzoio <raphaelzoio@hotmail.com>
2023-05-12 12:50:28 +0100

    Changed html attribute names

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -45,7 +45,7 @@ const Search = () => {
       <h1 className="text-4xl font-extrabold my-2 p-2 text-white">Search</h1>
       <form onSubmit={handleSubmit}>
         <label
-          for="default-search"
+          htmlFor="default-search"
           className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
         >
           Search
@@ -61,9 +61,9 @@ const Search = () => {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               ></path>
             </svg>
